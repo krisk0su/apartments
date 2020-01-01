db868a6c7e072fc6e3b5f47db3762c36de6c29e5
Christian <chrizkoof@gmail.com>
2020-01-01 10:18:53 -0800

    initial
    initial

---
+++ Apartments/Apartments_Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Apartments_Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
